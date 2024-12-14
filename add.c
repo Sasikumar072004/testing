4810477eec4062b584d1807148694df7bd6f8383
sasikumar072004 <sasi93602075@gmail.com>
2024-12-14 21:57:59 +0530

    change

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	int a, b, c;
+	scanf("%d", a);
+	printf("Enter the first number: ");
+	scanf("%d", b);
+	printf("Enter the second number: ");
+	c = a+ b;
+	printf("%d",c);
+	return 0;
+}
